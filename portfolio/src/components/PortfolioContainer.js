bc7310ae8d008e6cfa9e8e8c35751de99ff93cd4
DanielK <Danielkang13@gmail.com>
2022-10-02 15:42:45 -0700

    removed initial src folder, created new src with index.js, app.js, and components. Implemented basic react app that displays a single html element

---
+++ portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function PortfolioContainer() {
+    //TODO: Display Header (with navbar), about Me section as default, and the footer
+    // So should return 4 different components here
+    //header should have navigation component children?
+
+    console.log("passed container")
+
+    return (<h1>Display portfolio here!</h1>);
+}
+
+export default PortfolioContainer;
